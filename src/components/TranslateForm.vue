2677fba15f57bc4376d091dc4b130981b336ccd7
Dan Elis <danelis19@gmail.com>
2019-02-05 21:11:05 +0100

    TranslateForm button is calling correct method

--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -1,10 +1,29 @@
 <template>
-  <div id="translateForm">TRANSLATE FORM</div>
+  <div id="translateForm">TRANSLATE FORM
+    <form v-on:submit="formSubmit">
+      <!-- binds this input to a component property -->
+      <input type="text" v-model="textToTranslate" placeholder="Enter a Word...">
+      <input type="submit" value="Translate">
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "TranslateForm"
+  name: "TranslateForm",
+  data() {
+    return {
+      // we're binding our input text above to this method
+      textToTranslate: ""
+    };
+  },
+  methods: {
+    // prevent from submitting to a webpage
+    formSubmit(e) {
+      alert("Translated Word submitted");
+      e.preventDefault();
+    }
+  }
 };
 </script>
 
